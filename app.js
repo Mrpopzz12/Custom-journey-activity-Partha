e0c68a97a0d677bdb321c4837ac02c8380ffac9e
Parthasarathy <68933946+Mrpopzz12@users.noreply.github.com>
2026-05-18 13:36:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 // Basic config
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
